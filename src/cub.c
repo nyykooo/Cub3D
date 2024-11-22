0fec1a0e4856810172f764aa54ea7bd84b10cb9f
nyykooo <nicholas.cakla@outlook.com>
2024-11-22 20:41:20 +0000

    cub3d: initializing cub and modifying main to call the modified parse function

--- src/cub.c
+++ src/cub.c
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 19:44:27 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/11/22 19:49:40 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/11/22 20:29:50 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,14 @@ t_cub	*ft_get_cub(void)
 
 void	ft_init_cub(char *av)
 {
-	printf("Initializing cub %s\n", av);
+	t_cub	*cub;
+
+	cub = ft_get_cub();
+	cub->map = (t_map *)malloc(sizeof(t_map));
+	if (!cub->map)
+		ft_put_error_msg("Malloc failed", 1);
+	cub->map->rows = 0;
+	cub->map->cols = 0;
+	ft_parse_map(av);
+	printf("Rows: %d\n", cub->map->rows);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 12:09:38 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/11/22 19:50:48 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/11/22 20:32:54 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,8 @@ int	main(int ac, char **av)
 {
 	t_cub	*cub;
 
-	if (ft_parse_input(av, ac))
-	{
-		cub = ft_get_cub();
-		ft_init_cub(av[1]);
-		(void)cub;
-	}
-	else
-		return (1);
+	ft_parse_input(av, ac);
+	cub = ft_get_cub();
+	ft_init_cub(av[1]);
+	(void)cub;
 }
