30579d026ff1d1e6f3fa3926d2f590a082de27a6
nyykooo <nicholas.cakla@outlook.com>
2024-12-06 22:37:44 +0000

    cub3d: changing floor and ceiling colors, also creating ft_draw_scene which paint the background of the game

--- src/cub/mlx/image.c
+++ src/cub/mlx/image.c
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 18:14:37 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/12/06 18:46:39 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/12/06 22:34:21 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,37 +23,61 @@ static void	my_mlx_pixel_put(t_cub *cub, int x, int y, int color)
 	}
 }
 
-static void	ft_draw_map(t_cub *cub)
+static void	ft_draw_scene(t_cub *cub)
 {
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	x;
-	unsigned int	y;
+	t_color	*ceiling;
+	t_color	*floor;
+	int		i;
+	int		j;
 
+	ceiling = cub->map->texture->ceiling;
+	floor = cub->map->texture->floor;
 	i = -1;
-	while (++i < cub->map->rows)
+	while (++i < SCREEN_HEIGHT)
 	{
 		j = -1;
-		while (++j < cub->map->cols)
+		while (++j < SCREEN_WIDTH)
 		{
-			y = -1;
-			while (++y < 32)
-			{
-				x = -1;
-				while (++x < 32)
-				{
-					if (cub->map->map[i][j] == '1')
-						my_mlx_pixel_put(cub, j * 10 + x, i * 10 + y, 0x000000);
-					else if (cub->map->map[i][j] == '0')
-						my_mlx_pixel_put(cub, j * 10 + x, i * 10 + y, 0xFFFFFF);
-					else if (cub->map->map[i][j] == 'N')
-						my_mlx_pixel_put(cub, j * 10 + x, i * 10 + y, 0xFF0000);
-				}
-			}
+			if (i < (SCREEN_HEIGHT * 0.6))
+				my_mlx_pixel_put(cub, j, i, ceiling->color);
+			else
+				my_mlx_pixel_put(cub, j, i, floor->color);
 		}
 	}
 	mlx_put_image_to_window(cub->mlx_ptr, cub->win, cub->img, 0, 0);
 }
+
+// static void	ft_draw_map(t_cub *cub)
+// {
+// 	unsigned int	i;
+// 	unsigned int	j;
+// 	unsigned int	x;
+// 	unsigned int	y;
+
+// 	i = -1;
+// 	while (++i < cub->map->rows)
+// 	{
+// 		j = -1;
+// 		while (++j < cub->map->cols)
+// 		{
+// 			y = -1;
+// 			while (++y < 32)
+// 			{
+// 				x = -1;
+// 				while (++x < 32)
+// 				{
+// 					if (cub->map->map[i][j] == '1')
+// 						my_mlx_pixel_put(cub, j * 10 + x, i * 10 + y, 0x000000);
+// 					else if (cub->map->map[i][j] == '0')
+// 						my_mlx_pixel_put(cub, j * 10 + x, i * 10 + y, 0xFFFFFF);
+// 					else if (cub->map->map[i][j] == 'N')
+// 						my_mlx_pixel_put(cub, j * 10 + x, i * 10 + y, 0xFF0000);
+// 				}
+// 			}
+// 		}
+// 	}
+// 	mlx_put_image_to_window(cub->mlx_ptr, cub->win, cub->img, 0, 0);
+// }
 
 void	ft_image_hub(t_cub *cub)
 {
@@ -65,5 +89,5 @@ void	ft_image_hub(t_cub *cub)
 	cub->addr = MLX_GDA(cub->img, &cub->bpp, &cub->line_lenght, &cub->endian);
 	if (!cub->addr)
 		ERROR_PRINT(ERROR_MSG(1, ERROR_GDA), 1);
-	ft_draw_map(cub);
+	ft_draw_scene(cub);
 }
