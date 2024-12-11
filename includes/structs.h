81d85f19f1f59d6ad0d098bbb41ce848b1c73eb8
nyykooo <nicholas.cakla@outlook.com>
2024-12-11 15:12:27 +0000

    cub3d: including sprite study structures

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 19:39:51 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/12/06 17:09:23 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/12/11 15:10:55 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,69 @@
 
 # include "headers.h"
 
+
+// ------ sprite study --------
+
+
+typedef struct s_win
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int		width;	
+	int		height;	
+}		t_win;
+
+typedef struct s_image
+{
+	t_win	win;
+	void	*img_ptr;
+	char	*addr;
+	int		h;
+	int		w;
+	int		bpp;
+	int		endian;
+	int		line_len;
+}		t_image;
+
+
+enum entity {
+	PLAYER,
+	ENEMY,
+	EVENT,
+	OBJ,
+};
+
+typedef struct s_animation {
+	t_list *	frames;
+	int		width;
+	int		height;
+	int		delay;			// How many fps it takes to change animation
+	int		_tmp_delay;		// Delay Iterator
+	int		current_frame_num;	// Which frame is selected
+	long int	last_updated;		// When was the last update
+	long int	frame_count;		// The frame count
+	enum entity	entity;
+}		t_animation;
+
+
+typedef struct s_sprite {
+	t_list	* animations;
+	char	* name;
+	char	* file_path;
+	t_image	sprite_img;
+	int	width;
+	int	height;
+	int	z_index;
+}		t_sprite;
+
+typedef	struct sprite_slice {
+	int x;
+	int y;
+	int width;
+	int height;
+}	sprite_slice;
+
+// ----------------------
 typedef struct s_fixed
 {
 	const int	bits;
