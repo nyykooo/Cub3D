8cdb3e05d52fccef6eb20221cb3a8862303b545e
gitbrunoh <brunohenriqueufpr@gmail.com>
2025-01-02 14:54:58 +0000

    cub3d: shading 1.1 - performance adjust

--- src/cub/mlx/shading.c
+++ src/cub/mlx/shading.c
@@ -6,20 +6,18 @@
 /*   By: brunhenr <brunhenr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 13:12:33 by brunhenr          #+#    #+#             */
-/*   Updated: 2025/01/02 13:17:20 by brunhenr         ###   ########.fr       */
+/*   Updated: 2025/01/02 14:52:38 by brunhenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/headers.h"
 
-static int	ft_apply_shading(int color, double distance)
+static int	ft_apply_shading(int color, double shading_factor)
 {
-	double	shading_factor;
 	int		r;
 	int		g;
 	int		b;
 
-	shading_factor = 1.0 / (1.0 + distance * distance * SHADE_FACTOR);
 	r = ((color >> 16) & 0xFF) * shading_factor;
 	g = ((color >> 8) & 0xFF) * shading_factor;
 	b = (color & 0xFF) * shading_factor;
@@ -28,12 +26,15 @@ static int	ft_apply_shading(int color, double distance)
 
 void	ft_shading(int *buffer, int len, double orto_wall_dist)
 {
-	int	i;
+	int		i;
+	double	shading_factor;
 
 	i = 0;
+	shading_factor = 1.0 / (1.0 + orto_wall_dist * \
+	orto_wall_dist * SHADE_FACTOR);
 	while (i < len)
 	{
-		buffer[i] = ft_apply_shading(buffer[i], orto_wall_dist);
+		buffer[i] = ft_apply_shading(buffer[i], shading_factor);
 		i++;
 	}
 }
